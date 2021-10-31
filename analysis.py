cd67ac79397cc5029acfc2effcf0260424e43b42
P1umer <1226063714@qq.com>
2021-10-31 10:39:16 -0700

    Perform code refactoring on analysis.py

--- analysis.py
+++ analysis.py
@@ -26,83 +26,144 @@
 import json
 import sys
 
-func_list=[]
+func_info_list=[]
 
-func_name=None
-ida_num =0
-ida_identified_num = 0
-ida_unknown = 0 # part of ida_unidentified_num + ida_add
-ida_unidentified_num = 0 # dwarf_num - dwarf_none - ida_identified_num
-dwarf_num=0
-dwarf_none = 0
+class FunctionInfo:
 
-# ugly code
-def clear_cache():
-    global func_name,ida_num,ida_identified_num,ida_unknown,ida_unidentified_num,dwarf_num,dwarf_none
     func_name=None
     ida_num =0
     ida_identified_num = 0
-    ida_unknown = 0 
-    ida_unidentified_num = 0 
-    dwarf_num=0
-    dwarf_none = 0
+    ida_unknown = 0 # part of ida_unidentified_num + ida_add
+    ida_unidentified_num = 0 # dwarf_num - dwarf_none - ida_identified_num
+    dwarf_num = 0
+    dwarf_none_num = 0
+    
+    def set_func_name(self,name):
+        self.func_name = name
+    
+    def set_ida_num(self,num):
+        self.ida_num = num
+    
+    def get_ida_num(self):
+        return self.ida_num
+    
+    def set_ida_identified_num(self,num):
+        self.ida_identified_num = num
+    
+    def get_ida_identified_num(self):
+        return self.ida_identified_num
+    
+    def add_ida_identified_num(self,num):
+        self.ida_identified_num+=num
 
-def serialize():
-    global func_name,ida_num,ida_identified_num,ida_unknown,ida_unidentified_num,dwarf_num,dwarf_none
-    global func_list
-    return func_list.append({
-        "func_name":func_name,
-        "ida_num":ida_num,
-        "ida_identified_num":ida_identified_num,
-        "ida_unknown":ida_unknown,
-        "ida_unidentified_num":ida_unidentified_num,
-        "dwarf_num":dwarf_num,
-        "dwarf_none":dwarf_none
-    })
+    def set_ida_unknown(self,num):
+        self.ida_unknown = num
+    
+    def get_ida_unknown(self):
+        return self.ida_unknown
+    
+    def set_ida_unidentified_num(self,num):
+        self.ida_unidentified_num = num
+    
+    def get_ida_unidentified_num(self):
+        return self.ida_unidentified_num
+    
+    def set_dwarf_num(self,num):
+        self.dwarf_num = num
+    
+    def get_dwarf_num(self):
+        return self.dwarf_num
+    
+    def set_dwarf_none_num(self,num):
+        self.dwarf_none_num = num
+    
+    def get_dwarf_none_num(self):
+        return self.dwarf_none_num
+    
+    def serialize(self):
+        global func_info_list
+        return func_info_list.append({
+            "func_name":self.func_name,
+            "ida_num":self.ida_num,
+            "ida_identified_num":self.ida_identified_num,
+            "ida_unknown":self.ida_unknown,
+            "ida_unidentified_num":self.ida_unidentified_num,
+            "dwarf_num":self.dwarf_num,
+            "dwarf_none_num":self.dwarf_none_num
+        })
+    # def clear_cache(self):
+    #     self.func_name=None
+    #     self.ida_num =0
+    #     self.ida_identified_num = 0
+    #     self.ida_unknown = 0 
+    #     self.ida_unidentified_num = 0
+    #     self.dwarf_num=0
+    #     self.dwarf_none_num = 0
 
-def find_dwarf_func(dfunc_list,fname):
-    global func_name
-    func_name = fname
+
+
+
+# ugly code
+# def clear_cache():
+#     global func_name,ida_num,ida_identified_num,ida_unknown,ida_unidentified_num,dwarf_num,dwarf_none
+#     func_name=None
+#     ida_num =0
+#     ida_identified_num = 0
+#     ida_unknown = 0 
+#     ida_unidentified_num = 0 
+#     dwarf_num=0
+#     dwarf_none = 0
+
+# def serialize():
+#     global func_name,ida_num,ida_identified_num,ida_unknown,ida_unidentified_num,dwarf_num,dwarf_none
+#     global func_info_list
+#     return func_info_list.append({
+#         "func_name":func_name,
+#         "ida_num":ida_num,
+#         "ida_identified_num":ida_identified_num,
+#         "ida_unknown":ida_unknown,
+#         "ida_unidentified_num":ida_unidentified_num,
+#         "dwarf_num":dwarf_num,
+#         "dwarf_none":dwarf_none
+#     })
+
+def find_dwarf_func(function_info,dfunc_list,fname):
+    function_info.func_name = fname
     # search in dfunc_list
     candidates = [df for df in dfunc_list if df['function_name']==fname]
     # check number of candidates, must be 1
     assert len(candidates)<=1,"Error dwarf function number"
     return candidates[0]
     
-def analysis_dfunc(dfunc):
-    global dwarf_num,dwarf_none
-    dwarf_num = len(dfunc['var_list'])
-    dwarf_none = len([dv for dv in dfunc['var_list'] if dv['name']==""])
+def analysis_dfunc(finfo, dfunc):
+    finfo.set_dwarf_num(len(dfunc['var_list']))
+    finfo.set_dwarf_none_num(len([dv for dv in dfunc['var_list'] if dv['name']==""]))
     return
 
-def deep_analysis_ifunc():
-    global ida_num,ida_identified_num,ida_unknown,ida_unidentified_num,dwarf_num,dwarf_none
+def deep_analysis_ifunc(finfo):
     # maybe negative since ida_identified_num stands for number all IDENTIFIED vars
     # but the vars may be duplicate
-    ida_unidentified_num = dwarf_num - dwarf_none - ida_identified_num
-    ida_unknown = ida_num - ida_identified_num
+    finfo.set_ida_unidentified_num(finfo.get_dwarf_num() - finfo.get_dwarf_none() - finfo.get_ida_identified_num())
+    finfo.set_ida_unknown(finfo.get_ida_num() - finfo.get_ida_identified_num())
 
-def intersect_varlist(ilist,dlist):
-    global ida_identified_num,func_name
+def intersect_varlist(finfo,ilist,dlist):
     var_map={}
     for dl in dlist:
         var_map[dl['name']]=1
-    print(func_name)
     for il in ilist:
         print(il)
         if il['name']=='' or il['isargs']==True or il['hasdwarf']==False:
             continue
         # assert var_map[il['name']],"IDA must has symbol here"
-        ida_identified_num+=1
+        finfo.add_ida_identified_num(1)
     return 
 
-def analysis_ifunc(ifunc,dfunc):
-    global ida_num,ida_identified_num
-    ida_num = len(ifunc['var_list'])
-    intersect_varlist(
+def analysis_ifunc(finfo,ifunc,dfunc):
+    finfo.set_ida_num(len(ifunc['var_list']))
+    intersect_varlist(finfo,
         ifunc['var_list'],
         dfunc['var_list'])
-    deep_analysis_ifunc()
+    deep_analysis_ifunc(finfo)
     return 
 
 def analysis(ijson_file,djson_file):
@@ -110,15 +171,15 @@ def analysis(ijson_file,djson_file):
     with open(ijson_file, 'rb') as ijson, open(djson_file, 'rb') as djson:
         ifunc_list,dfunc_list= json.load(ijson),json.load(djson)
         for ifunc in ifunc_list:
-            clear_cache()
+            finfo = FunctionInfo()
             try:
-                dfunc = find_dwarf_func(dfunc_list,ifunc['function_name'])
+                dfunc = find_dwarf_func(finfo,dfunc_list,ifunc['function_name'])
             except:
                 print("[*] Cannot find function {} in dwarf".format(ifunc['function_name']))
                 continue
-            analysis_dfunc(dfunc)
-            analysis_ifunc(ifunc,dfunc)
-            serialize()
+            analysis_dfunc(finfo,dfunc)
+            analysis_ifunc(finfo,ifunc,dfunc)
+            finfo.serialize()
         
 
 if __name__ == "__main__":
@@ -127,7 +188,7 @@ if __name__ == "__main__":
     ida_json_file,dwarf_json_file = binary+'.ida.json',binary+'.dwarf.json'
     with open('{}.ans.json'.format(binary), 'w+') as f:
         analysis(ida_json_file,dwarf_json_file)
-        json.dump(func_list,f)
+        json.dump(func_info_list,f)
 
 
 
