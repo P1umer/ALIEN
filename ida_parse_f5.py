813767185231c10678e27eec3ec0ae5cd17d922d
P1umer <1226063714@qq.com>
2021-11-05 04:55:36 -0700

    Perform code refactoring on ida_parse_f5.py

--- ida_parse_f5.py
+++ ida_parse_f5.py
@@ -2,11 +2,128 @@ from idautils import *
 from idc import *
 from idaapi import *
 import idc, idaapi, idautils, ida_xref
-import pickle
 import json
 import sys
 
 func_list=[]
+
+class ModuleInfo:
+
+    ida_num_total = 0
+    ida_has_dwarf_total = 0
+    ida_reg_num_total = 0
+    ida_stack_num_total = 0
+    # ida_poly_num_total = 0
+    ida_unknown_num_total = 0
+    func_info_list = []
+
+    def add_func_info(self,func_name,
+                    ida_num,ida_has_dwarf,
+                    ida_reg_num,
+                    ida_stack_num,
+                    # dwarf_poly_num,
+                    ida_unknown_num,
+                    var_list):
+
+        self.ida_num_total+=ida_num
+        self.ida_has_dwarf_total+=ida_has_dwarf
+        self.ida_reg_num_total+=ida_reg_num
+        self.ida_stack_num_total+=ida_stack_num
+        # self.dwarf_poly_num_total+=dwarf_poly_num
+        self.ida_unknown_num_total+=ida_unknown_num
+
+        return self.func_info_list.append({
+            "function_name":func_name,
+            "ida_num":ida_num,
+            "ida_has_dwarf":ida_has_dwarf,
+            "ida_reg_num":ida_reg_num,
+            "ida_stack_num":ida_stack_num,
+            "ida_unknown_num":ida_unknown_num,
+            "var_list":var_list
+        })
+
+    def serialize(self):
+        return {
+            "ModuleInfo":{
+                "ida_num_total":self.ida_num_total,
+                "ida_has_dwarf_total":self.ida_has_dwarf_total,
+                "ida_reg_num_total":self.ida_reg_num_total,
+                "ida_stack_num_total":self.ida_stack_num_total
+            },
+            "FunctionInfo":self.func_info_list
+        }
+
+class FunctionInfo:
+    func_name=None
+    ida_num = 0
+    ida_has_dwarf = 0
+    ida_reg_num = 0
+    ida_stack_num = 0
+    # ida_poly_num = 0
+    ida_unknown_num = 0
+    var_list = []
+    code = None
+
+    def __init__(self):
+        self.func_name=None
+        self.ida_num = 0
+        self.ida_has_dwarf =0
+        self.ida_reg_num = 0
+        self.ida_stack_num = 0
+        self.ida_unknown_num =0
+        self.var_list = []
+        self.code=None
+
+    
+    def set_func_name(self,name):
+        self.func_name = name
+
+    def set_func_code(self,code):
+        self.code = code
+    
+    def add_ida_num(self):
+        self.ida_num+=1
+        return True
+
+    def __add_ida_reg_num(self):
+        self.ida_reg_num+=1
+    
+    def __add_ida_stack_num(self):
+        self.ida_stack_num+=1
+    
+    def __add_ida_unknown_num(self):
+        self.ida_unknown_num+=1
+
+    
+    def __statistics(self,ltype):
+        self.ida_num+=1
+        # assert (ltype!="None"),"ida variable is neither in STACK nor in REGISTER"
+        return {
+            "None": (self.__add_ida_unknown_num),
+            "Reg": (self.__add_ida_reg_num),
+            "Stack": (self.__add_ida_stack_num),
+            # "Poly": (self.__add_ida_poly_num)
+        }[ltype]()
+    
+    def add_var_info(self,vname,isargs,ltype):
+        self.__statistics(ltype)
+        self.var_list.append({
+            "name":vname,
+            "isargs":isargs,
+            "type":ltype
+        })
+    
+    def serialize(self,minfo):
+        return minfo.add_func_info(
+            self.func_name,
+            self.ida_num,
+            self.ida_has_dwarf,
+            self.ida_reg_num,
+            self.ida_stack_num,
+            # self.ida_poly_num,
+            self.ida_unknown_num,
+            self.var_list
+        )
 
 def serialize(fname,vlist,code):
     global func_list
@@ -23,19 +140,25 @@ def ida_get_code(cfunc):
     lines = [tag_remove(sline.line) for sline in sv]
     return "".join(lines)
 
+def check_location_type(lv):
+    ref = 0
+    if lv.is_reg_var():
+        ref = ref | 1
+    if lv.is_stk_var():
+        ref = ref | 2
+    data=['None','Reg',"Stack","Poly"]
+    return data[ref]
 
-def ida_get_lvars(cfunc):
+def ida_get_lvars(cfunc,finfo):
     if cfunc is None:
         return False
     lvars = cfunc.get_lvars()
-    vlist=[{
-        "name":lv.name,
-        "hasdwarf":lv.has_user_info,
-        "isargs":lv.is_arg_var,
-        "isreg":lv.is_reg_var()
-        } for lv in lvars]
-    # print(lv.name,lv.has_user_info)
-    return vlist
+    for lv in lvars:
+        ltype = check_location_type(lv)
+        finfo.add_var_info(lv.name,
+                        lv.is_arg_var,
+                        ltype)
+
 
 def ida_decompile_func(ea):
     if not init_hexrays_plugin():
@@ -52,8 +175,10 @@ def ida_decompile_func(ea):
 
 def start_traverse(filename):
     global func_list
+    minfo = ModuleInfo()
     with open('{}.ida.json'.format(filename), 'w+') as f:
         for fn in idautils.Functions():
+            finfo = FunctionInfo()
             flags = get_func_flags(fn)
               # Ignore THUNK (jump function) or library functons
             if flags & FUNC_LIB or flags & FUNC_THUNK:
@@ -62,14 +187,13 @@ def start_traverse(filename):
             if not func:
                 continue
             try:
-                serialize(
-                    get_func_name(fn),
-                    ida_get_lvars(func),
-                    ida_get_code(func)
-                )
+                finfo.set_func_name(get_func_name(fn))
+                finfo.set_func_code(ida_get_code(func))
+                ida_get_lvars(func,finfo)
+                finfo.serialize(minfo)
             except IOError:
                 raise RuntimeError('IO Error')
-        json.dump(func_list,f)
+        json.dump(minfo.serialize(),f)
     return 
 
 # ./idat64 -c -S"/home/p1umer/Documents/dwarf_research/ida_parse_f5.py" ~/Documents/dwarf_research/database/O0/mujs/build/debug/mujs
